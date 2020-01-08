6c87d763c1c4d0c5bf748f6ea143404d7e6758b7
Scott Kim <scott102938@gmail.com>
2020-01-08 14:43:55 +0000

    Soem start but it's not working

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# 1. Download dotfiles
+git clone https://github.com/scott0129/dotfiles.git ~/dotfiles
+
+find ~/dotfiles/ -maxdepth 1 -type f -exec cp {} ~/ \;	# Copy all files from git repo into home dir
+
+
+# 2. Install Neovim and plugins
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim		# This might also install python.
+
+# Make nvim refer to same .vimrc
+mkdir -p ~/.config/nvim && echo -e "set runtimepath^=~/.vim runtimepath+=~/.vim/after \nlet &packpath=&runtimepath \nsource ~/.vimrc" >> ~/.config/nvim/init.vim
+
+# Install ctags	plugin
+sudo apt-get install exuberant-ctags
+
+
+# 3. Install zsh
+sudo apt-get install zsh
+chsh -s $(which zsh)
+
+# 4. Install Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv .zshrc.pre-oh-my-zsh .zshrc
