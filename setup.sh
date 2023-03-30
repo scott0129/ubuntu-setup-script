d2522322bbdc061725d557bd6f79ca3ae0d374b9
Scott Kim <scott102938@gmail.com>
2023-03-29 17:44:19 -0700

    Install TPM

--- setup.sh
+++ setup.sh
@@ -12,23 +12,25 @@ sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim		# This might also install python.
 
+# 3. Install Tmux Plugin Manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 # Make nvim refer to same .vimrc
 mkdir -p ~/.config/nvim && echo -e "set runtimepath^=~/.vim runtimepath+=~/.vim/after \nlet &packpath=&runtimepath \nsource ~/.vimrc" >> ~/.config/nvim/init.vim
 
 # Install ctags	plugin
 sudo apt-get install exuberant-ctags
 
-
-# 3. Install zsh
+# 4. Install zsh
 sudo apt-get install zsh
 chsh -s $(which zsh)
 
-# 4. Install Oh-My-Zsh
+# 5. Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# 5.1 Install Nerd font (TBD)
+# 6.1 Install Nerd font (TBD)
 
-# 5.2 Install Starship prompt
+# 6.2 Install Starship prompt
 curl -sS https://starship.rs/install.sh | sh
 
 mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
